b2e528b8a83d04b1ff11902065e72e39166a1e9f
Smriti Srikanth <smritisrikanth@Smritis-MBP.fios-router.home>
2023-03-23 23:53:22 -0400

    done

---
+++ one_simulation_test.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+library(r2r)
+library(qfm)
+library(furrr)
+library(rgl)
+library(treespace)
+setwd('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo')
+
+# get job id from system 
+job_id = 49
+
+# construct parameter table 
+param_tb <- read.table('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/param_tb.txt',header = T)
+
+#load file
+filename <- paste('./input', param_tb$input_file[job_id], sep = "")
+if (file.exists(filename)) {
+    load(filename)
+} else {
+    stop()
+}
+
+#initialize parameters
+total_bp <- 10^9
+mut_rate <- param_tb$mutation_rate[job_id]
+num_mut <- 0
+mut_list <- c()
+node_mut_list <- hashmap()
+
+in_node <- count_graph$phylo_edges$in_node
+edge_length <- count_graph$phylo_edges$length
+node_mut_list[[names(in_node)[1]]] <- character()
+i <- 1
+
+#generate mutations
+for (d in names(in_node)) {
+  node <- in_node[[d]]
+  if (is.null(node_mut_list[[node]])) {
+    node_mut_list[[node]] <- character()
+  }
+  node_mut_list[[d]] <- node_mut_list[[node]]
+  for (i in 1:rbinom(1,total_bp,1-exp(-mut_rate * edge_length[i]))) {
+    mut <- paste0(sample(c(letters, 1:9), size = 20, replace = T), collapse = "")
+    mut_list <- c(mut_list, mut)
+    node_mut_list[[d]] <- c(node_mut_list[[d]], mut)
+    num_mut <- num_mut + 1
+  }
+  i <- i+1
+}
+
+#make cell mutation table of tips
+cell_mut_tb = bind_rows(map(tr$tip.label, function(tip_id) {
+  tibble(cell = tip_id,
+         mut = node_mut_list[[tip_id]],
+         value = 1)
+}))
+
+#subset to mutations that occur in more than one cell
+occurrences <- table(cell_mut_tb$mut)
+subset_mut <- occurrences[occurrences > 1]
+cell_mut_tb <- subset(cell_mut_tb, mut %in% names(subset_mut))
+
+#formatting and reconstruction
+m <- spread(cell_mut_tb, mut, value, fill = 0)
+m$type <- m$cell
+m <- m[,c(1,ncol(m),3:ncol(m))]
+
+chr_mat = as.matrix(m[-c(1:2)])
+rownames(chr_mat) = m$cell
+
+plan(multisession, workers = 8)
+
+mut_p = estimate_mut_p(chr_mat, t_total = res2$total_time - tr$root.edge)
+mut_p$mut_rate = rep(mut_rate, ncol(m)-2)
+
+tr2 = phylotime(chr_mat, t_total = res2$total_time - tr$root.edge)
+tr2$root.edge <- res2$total_time - tr$root.edge
+
+#calculate distances
+kc0 <- treeDist(tr,tr2, lambda = 0)
+kc1 <- treeDist(tr,tr2, lambda = 1)
+
+#save results
+result <- paste(param_tb$sample_size[job_id], param_tb$sampling[job_id], mut_rate, filename, job_id, num_mut, kc0, kc1, sep = '\t')
+system(paste("echo",result,'>> /home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/results.txt'))
+file = paste(filename, mut_rate, param_tb$num_sim_2, '.rda', sep = "")
+save(tr2, file = paste('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/', file, sep = ""))
