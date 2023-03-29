06764bdd7bae52452d5a96cc789118fb04ff245f
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2023-03-29 17:06:43 -0400

    copied over from one sim test

--- somatic_mut_sim.R
+++ somatic_mut_sim.R
@@ -1,26 +1,35 @@
+sink(stdout(), type="message")
+
 library(tidyverse)
 library(r2r)
 library(qfm)
 library(furrr)
-library(rgl)
+#library(rgl)
 #library(treespace)
 library(TreeDist)
 setwd('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo')
+print('success')
 
 # get job id from system 
 job_id = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
 
 # construct parameter table 
+
 param_tb <- read.table('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/param_tb.txt',header = T)
+print('success')
 
 #load file
-filename <- paste('./input', param_tb$input_file[job_id], sep = "")
+filename <- paste('./input/yi_output/', param_tb$input_file[job_id], sep = "")
+print('success')
+print(filename)
+
 if (file.exists(filename)) {
     load(filename)
 } else {
+    print('file not found')
     stop()
 }
-
+print('success')
 #initialize parameters
 total_bp <- 10^9
 mut_rate <- param_tb$mutation_rate[job_id]
@@ -29,6 +38,7 @@ mut_list <- c()
 node_mut_list <- hashmap()
 
 in_node <- count_graph$phylo_edges$in_node
+print('success')
 edge_length <- count_graph$phylo_edges$length
 node_mut_list[[names(in_node)[1]]] <- character()
 i <- 1
@@ -69,19 +79,24 @@ m <- m[,c(1,ncol(m),3:ncol(m))]
 chr_mat = as.matrix(m[-c(1:2)])
 rownames(chr_mat) = m$cell
 
-plan(multisession, workers = 8)
+plan(multisession, workers = 16)
+print('success')
 
 mut_p = estimate_mut_p(chr_mat, t_total = res2$total_time - tr$root.edge)
 mut_p$mut_rate = rep(mut_rate, ncol(m)-2)
 
 tr2 = phylotime(chr_mat, t_total = res2$total_time - tr$root.edge)
+print('phylotime done')
 tr2$root.edge <- res2$total_time - tr$root.edge
 
 #calculate distances
 kc0 <- KendallColijn(tr,tr2)
+print('success')
 
 #save results
-result <- paste(param_tb$sample_size[job_id], param_tb$sampling[job_id], mut_rate, filename, job_id, num_mut, kc0, kc0, sep = '\t')
-system(paste("echo",result,'>> /home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/results.txt'))
-file = paste(filename, mut_rate, param_tb$num_sim_2, '.rda', sep = "")
-save(tr2, file = paste('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/', file, sep = ""))
+result <- paste(param_tb$sample_size[job_id], param_tb$sampling[job_id], mut_rate, filename, job_id, num_mut, kc0, kc0, sep = '  ')
+print('success')
+system(paste("echo ",result,' >> /home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/results.txt', sep = ""))
+output_filename <- paste('./output/', param_tb$input_file[job_id], '_', mut_rate, '_', param_tb$num_sim_2[job_id], '.rda', sep = "")
+print(output_filename)
+save(tr2, list = c("tr2"), file = output_filename)
