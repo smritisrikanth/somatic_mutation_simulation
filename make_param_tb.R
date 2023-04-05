ecb151105359f5d807b55551e5206865984697b5
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2023-04-05 13:47:48 -0400

    made changes for second simulation

--- make_param_tb.R
+++ make_param_tb.R
@@ -3,11 +3,11 @@ library(tidyverse)
 param_tb = as_tibble(expand.grid(mutation_rate = c(1e-8,1e-9,1e-10),
                                  sampling = c('fixed', 'proportional'), sample_size = c(50,100,200), num_sim_2 = 1:2, num_sim_1 = 1:20))
 
-param_tb$input_file = paste('sample_size_', param_tb$sample_size, '_', param_tb$sampling, '_', floor((as.numeric(rownames(param_tb))+2)/3), '.rda', sep = "")
+param_tb$input_file = paste('sample_size_', param_tb$sample_size, '_', param_tb$sampling, '_', floor((as.numeric(rownames(param_tb))+2)/3), '.rda_', param_tb$mutation_rate, '_', param_tb$num_sim2, '.rda', sep = "")
 
-write.table(as.data.frame(param_tb),'/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/param_tb.txt',col.names = T,row.names = F,quote = F)
+write.table(as.data.frame(param_tb),'/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output2/param_tb_2.txt',col.names = T,row.names = F,quote = F)
 
-results <- data.frame(matrix(nrow=0, ncol = 8))
-colnames(results) <- c('sample_size', 'sampling', 'mutation_rate', 'input_file', 'job_id', 'num_mut', 'KC0', 'KC1')
+# results <- data.frame(matrix(nrow=0, ncol = 8))
+# colnames(results) <- c('sample_size', 'sampling', 'mutation_rate', 'input_file', 'job_id', 'num_mut', 'KC0', 'KC1')
 
-write.table(results,'/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/results.txt',col.names = T,row.names = F,quote = F)
+# write.table(results,'/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output2/results_2.txt',col.names = T,row.names = F,quote = F)
---
+++ simulation_barcodes.R
@@ -0,0 +1,58 @@
+sink(stdout(), type="message")
+
+# simulate lineage barcodes
+library(ape)
+library(qfm)
+library(tidyverse)
+library(igraph)
+library(ggraph)
+setwd('/home/ssrikan2/data-kreza1/smriti/qfm2')
+# devtools::load_all()
+
+#job_id = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+job_id = 49
+input_path <- '/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/'
+param_table <- read.table('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/output2/param_tb_2.txt',header = T)
+load(paste0(input_path,param_table$input_file[job_id]))
+global_step_size = 0.01
+global_target_time = 9.0
+# mut_p = readRDS("./metadata//mut_p_marc1.rds")
+
+# # simulate barcodes
+# chr_mat = simulate_all_phylogeny_bb(count_graph, mut_p)
+
+# # reconstruct phylogeny
+library(furrr)
+# plan(multisession, workers = 12)
+# tr2 = phylotime(sc_mat = chr_mat, mut_p = mut_p,
+#                  t_total = count_graph$target_time - count_graph$phylo_edges$length[1],
+#                  parallel = T)
+
+# x = cophenetic(tr_r)
+# y = cophenetic(tr)
+# tibble(a = c(x[rownames(y), colnames(y)]),
+#        b = c(y)) %>%
+#         sample_n(size = 1e4) %>%
+#         ggscatter(x = "a", y = "b") + geom_abline()
+
+# reconstruct fate map using reconstructed phylogeny
+res3 = ice_fase_mod(tr = tr2,
+                    sc_celltypes = get_type_from_id(tr2$tip.label),
+                    root_time = count_graph$phylo_edges$out_time[1],
+                    total_time = 9.0 - count_graph$phylo_edges$out_time[1])
+res4 = collaspe_di(res3, 0.2)
+res5 = ice_fase_mutlifurc(tr = tr2,
+                          sc_celltypes = get_type_from_id(tr2$tip.label),
+                          root_time = count_graph$phylo_edges$out_time[1],
+                          total_time = 9.0 - count_graph$phylo_edges$out_time[1],
+                          gr = res4$gr)
+save(count_graph, tr, res0, res1, res2, res3, res4, res5, 
+     file = paste0("/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output2/",job_id,'-',param_table$input_file[job_id]))
+
+
+
+
+
+
+
+
