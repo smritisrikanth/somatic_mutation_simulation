4bbd168cbfac180b5b1c34c50496fa2a2409c514
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2023-04-06 03:11:12 -0400

    adjusted for correctness simulation

--- make_param_tb.R
+++ make_param_tb.R
@@ -1,14 +1,14 @@
 library(tidyverse)
 
 param_tb = as_tibble(expand.grid(mutation_rate = c(1e-8,1e-9,1e-10),
-                                 sampling = c('fixed', 'proportional'), sample_size = c(50,100,200), num_sim_2 = 1:2, num_sim_1 = 1:20))
+                                 sampling = c('fixed', 'proportional'), sample_size = c(50,100,200), reconstruction_num = c('res0', 'res1'), num_sim_1 = 1:20))
 
-param_tb$input_file = paste('sample_size_', param_tb$sample_size, '_', param_tb$sampling, '_', floor((as.numeric(rownames(param_tb))+2)/3), '.rda_', param_tb$mutation_rate, '_', param_tb$num_sim_2, '.rda', sep = "")
-param_tb$input_file_original = paste('sample_size_', param_tb$sample_size, '_', param_tb$sampling, '_', floor((as.numeric(rownames(param_tb))+2)/3), '.rda', sep = "")
+param_tb$input_file = paste('sample_size_', param_tb$sample_size, '_', param_tb$sampling, '_', floor((as.numeric(rownames(param_tb))+5)/6), '.rda', sep = "")
+#param_tb$input_file_original = paste('sample_size_', param_tb$sample_size, '_', param_tb$sampling, '_', floor((as.numeric(rownames(param_tb))+2)/3), '.rda', sep = "")
 
-write.table(as.data.frame(param_tb),'/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output2/param_tb_2.txt',col.names = T,row.names = F,quote = F)
+write.table(as.data.frame(param_tb),'/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/correctness_output/param_tb_3.txt',col.names = T,row.names = F,quote = F)
 
-# results <- data.frame(matrix(nrow=0, ncol = 8))
-# colnames(results) <- c('sample_size', 'sampling', 'mutation_rate', 'input_file', 'job_id', 'num_mut', 'KC0', 'KC1')
+results <- data.frame(matrix(nrow=0, ncol = 7))
+colnames(results) <- c('sample_size', 'sampling', 'input_file', 'reconstruction #', 'job_id', '% correct', '% complete')
 
-# write.table(results,'/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output2/results_2.txt',col.names = T,row.names = F,quote = F)
+write.table(results,'/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/correctness_output/results_2.txt',col.names = T,row.names = F,quote = F)
