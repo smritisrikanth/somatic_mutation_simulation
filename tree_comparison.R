285113d34856a7a60327b842ba7269d7fe1d4815
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2023-04-06 03:35:29 -0400

    tree comp script

---
+++ tree_comparison.R
@@ -0,0 +1,116 @@
+sink(stdout(), type="message")
+
+library(tidyverse)
+library(r2r)
+library(qfm)
+library(furrr)
+#library(rgl)
+#library(treespace)
+library(TreeDist)
+library(igraph)
+library(ggraph)
+setwd('/home/ssrikan2/data-kreza1/smriti/qfm2')
+devtools::load_all()
+
+setwd('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo')
+
+job_id = 49
+#job_id = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+
+# construct parameter table 
+
+param_tb <- read.table('/home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/output/param_tb.txt',header = T)
+filename <- paste('./input/yi_output/', param_tb$input_file[job_id], sep = "")
+if (file.exists(filename)) {
+    load(filename)
+} else {
+    print('file not found')
+    stop()
+}
+
+#functions
+
+compare_lists <- function (l1, l2) {
+  if (length(intersect(l1,l2) == length(l1)) && length(intersect(l1,l2) == length(l2))) {
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
+compare_partitions <- function (p1, p2) {
+  count = 0
+  for (node1 in p1) {
+    for (node2 in p2) {
+      l1 <- node1[[1]]
+      l2 <- node1[[2]]
+      l3 <- node2[[1]]
+      l4 <- node2[[2]]
+      if (compare_lists(l1,l3) && compare_lists(l2,l4)) {
+        count = count+1
+        break
+      }
+      if (compare_lists(l1,l4) && compare_lists(l2,l3)) {
+        count = count+1
+        break
+      }
+    }
+  }
+  return(c(count, length(p1), length(p2)))
+}
+
+get_partitions <- function(gr) {
+        obj = list_dd_and_tips_mod2(gr)
+        dd_list = obj$dd
+        tips_list = obj$tips
+        gr_tips = gr$tip.label
+        names(gr_tips) = gr_tips
+        tips_list = c(tips_list, gr_tips)
+        part_list = map(dd_list, function(dd_vec) {
+                tips_list[dd_vec]
+        })
+        part_list
+}
+
+compare_partitions <- function (p1, p2) {
+  count = 0
+  for (node1 in p1) {
+    for (node2 in p2) {
+      l1 <- node1[[1]]
+      l2 <- node1[[2]]
+      l3 <- node2[[1]]
+      l4 <- node2[[2]]
+      if (compare_lists(l1,l3) && compare_lists(l2,l4)) {
+        count = count+1
+        break
+      }
+      if (compare_lists(l1,l4) && compare_lists(l2,l3)) {
+        count = count+1
+        break
+      }
+    }
+  }
+  return(c(count, length(p1), length(p2)))
+}
+
+#calculations
+p <- get_partitions(phy)
+p0 <- get_partitions(res0)
+p1 <- get_partitions(res1)
+
+r0 <- compare_partitions(p, p0)
+r1 <- compare_partitions(p, p1)
+
+r0_correct <- (r0[3] - r0[1])/r0[3]
+r1_correct <- (r1[3] - r1[1])/r1[3]
+r0_complete <- (r0[2] - r0[1])/r0[2]
+r1_complete <- (r1[2] - r1[1])/r1[2]
+
+
+
+#save results
+result0 <- paste(param_tb$sample_size[job_id], param_tb$sampling[job_id], filename, 'res0', job_id, r0_correct, r0_complete, sep = '  ')
+result1 <- paste(param_tb$sample_size[job_id], param_tb$sampling[job_id], filename, 'res1', job_id, r1_correct, r1_complete, sep = '  ')
+
+system(paste("echo ",result,' >> /home/ssrikan2/data-kreza1/smriti/somatic_mut_sim/git_repo/correctness_output/results_2.txt', sep = ""))
+
+
